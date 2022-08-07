1dc438e96b0dc52c94885a734cabc63f07210468
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2022-08-07 08:52:13 -0300

    adicionar arquivo .tf do cluster k8s na digitalocean

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s_kubenews" {
+  name    = var.k8s_name
+  region  = var.region
+  # latest slug from the command 'doctl kubernetes options versions'
+  version = "1.23.9-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-1vcpu-2gb"
+    # auto_scale = false  # (default)
+    node_count  = 1
+  }
+}
+
+# node_pool 1
+# resource "digitalocean_kubernetes_node_pool" "node_pool1" {
+#   cluster_id = "digitalocean_kubernetes_cluster.k8s_kubenews.id"
+#   name       = "autoscale-worker-pool1"
+#   size       = "s-1vcpu-2gb"
+#   auto_scale = true
+#   min_nodes  = 1
+#   max_nodes  = 2
+# }
+
+variable "do_token" {
+    type=string
+}
+variable "k8s_name" {
+    type=string
+}
+variable "region" {
+    type=string
+}
+
+# # mostrar endpoint apos rodar "kubectl apply"
+# # obs: nao usar output para mostrar senhas nem outros dados sensiveis
+# output "kube_endpoint" {
+#   value = digitalocean_kubernetes_cluster.k8s_kubenews.endpoint
+# }
+
+resource "local_file" "kube_config" {
+  content = digitalocean_kubernetes_cluster.k8s_kubenews.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
