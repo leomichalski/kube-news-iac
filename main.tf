07fa60a698166f9659960b274c562da316fa4ff8
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2022-08-07 11:01:55 -0300

    formatar terraform com 'terraform fmt'

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
+}
+
+variable "do_token" {
+  type = string
+}
+variable "k8s_name" {
+  type = string
+}
+variable "region" {
+  type = string
 }
 
 provider "digitalocean" {
@@ -12,16 +22,15 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_kubernetes_cluster" "k8s_kubenews" {
-  name    = var.k8s_name
-  region  = var.region
+  name   = var.k8s_name
+  region = var.region
   # latest slug from the command 'doctl kubernetes options versions'
   version = "1.23.9-do.0"
 
   node_pool {
     name       = "worker-pool"
     size       = "s-1vcpu-2gb"
-    # auto_scale = false  # (default)
-    node_count  = 1
+    node_count = 1
   }
 }
 
@@ -35,16 +44,6 @@ resource "digitalocean_kubernetes_cluster" "k8s_kubenews" {
 #   max_nodes  = 2
 # }
 
-variable "do_token" {
-    type=string
-}
-variable "k8s_name" {
-    type=string
-}
-variable "region" {
-    type=string
-}
-
 # # mostrar endpoint apos rodar "kubectl apply"
 # # obs: nao usar output para mostrar senhas nem outros dados sensiveis
 # output "kube_endpoint" {
@@ -52,6 +51,6 @@ variable "region" {
 # }
 
 resource "local_file" "kube_config" {
-  content = digitalocean_kubernetes_cluster.k8s_kubenews.kube_config.0.raw_config
+  content  = digitalocean_kubernetes_cluster.k8s_kubenews.kube_config.0.raw_config
   filename = "kube_config.yaml"
 }
